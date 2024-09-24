f2a876fba2704e2a60e2285ecd947be4d90a1b95
c0b23125 <c0b23125a1@edu.teu.ac.jp>
2024-09-24 15:27:23 +0900

    練習２

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,18 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((300, 600))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kt_img = pg.image.load("fig/3.png")
+    kt_img = pg.transform.flip(kt_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kt_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
