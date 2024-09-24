b567e2169c42833476861399e1e82017def097d7
c0b23125 <c0b23125a1@edu.teu.ac.jp>
2024-09-24 16:05:16 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,20 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
+    bj_img = pg.transform.flip(bg_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
+        screen.blit(bj_img, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bj_img, [x+4800, 0])
         screen.blit(kt_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10000)
+        clock.tick(800)
 
 
 if __name__ == "__main__":
